c7952c0ee837a7e3fa2f150bafb205ea28510e56
Cameron Sivo <36455966+csiv23@users.noreply.github.com>
2018-04-10 11:31:08 -0400

    a
    a

---
+++ 2018-2019 Stuff/Speaker/speakerStuff.c
@@ -0,0 +1,7 @@
+
+task main()
+{
+	//Play a Sound File (need to use the File Management to Upload First)
+	playSoundFile("nfllll.wav");
+	wait1Msec(15000);
+}
---
+++ 2018-2019 Stuff/Speaker/speakerTest.c
@@ -0,0 +1,7 @@
+task main()
+{
+    while(1){
+        playSoundFile("testtone.wav");
+        wait1Msec(5000);
+    }
+}
